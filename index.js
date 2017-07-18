b77188c3e377bf2797507ca96de4b312f4b9b325
Mike Archie <rusty505@yahoo.com>
2017-07-18 16:40:16 -0700

    adds details (search results) w/ template

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(require("body-parser").urlencoded({extended: true}));
 // var http = require('http');
 // var fs = require('fs');
 // var querystring = require('querystring');
-var housing = require("./lib/hotels");
+var hotels = require("./lib/hotels");
 
 app.engine(".html", handlebars({extname: '.html'}));
 app.set("view engine", ".html");
@@ -25,65 +25,18 @@ app.get('/', function(req, res){
 //Express has an ejs
 app.post('/detail', function(req,res) {
     console.log(req.body);
-    console.log(req.body.mySearchText);
-    var result = housing.get(req.body.mySearchText);
+    console.log(req.body.hotelSearchText);
+    var result = hotels.get(req.body.hotelSearchText);
     console.log(result);
-    res.render("detail", {Name: req.body.mySearchText, result: result, hotels: housing.getAll()});
-
+    res.render("details", {name: req.body.hotelSearchText, result: result, hotels: hotels.getAll()});
 });
 
+// app.post('/delete', function(req,res) {
+//     // res.writeHead(200, {'Content-Type': 'text/plain'});
+//     // let result1 = hotels.delete(params.name);
+//     // let resultString1 = ((result1) ? JSON.stringify(result1) + " removed. ":
+//     //     "Hotel not found. Nothing deleted. ") + hotels.count() + " total hotels.";
+//     // res.render(resultString1);
+// });
+
 app.listen(3000);
-
-
-//
-// function serveStatic(res, path, contentType, responseCode) {
-//     if(!responseCode) responseCode = 200;
-//     console.log(__dirname + path);
-//     fs.readFile(__dirname + path, function(err, data){
-//         if(err) {
-//             res.writeHead(500, {'Content-Type': 'text/plain'});
-//             res.end('Internal Server Error');
-//         }
-//         else {
-//             res.writeHead(responseCode, {'Content-Type': contentType});
-//             res.end(data);
-//         }
-//     })
-// }
-//
-// http.createServer(function(req,res) {
-//     let url = req.url.split("?");
-//     let params = querystring.parse(url[1]);
-//     let path = url[0].toLowerCase();
-//
-//     switch(path) {
-//     case '/':
-//         serveStatic(res, '/public/home.html', 'text/html');
-//         break;
-//     case '/about':
-//         serveStatic(res, '/package.json', 'text/plain');
-//         break;
-//     case '/get':
-//         res.writeHead(200, {'Content-Type': 'text/plain'});
-//         let result = housing.get(params.name);
-//         let resultString = result ? JSON.stringify(result) : "Hotel not found";
-//         res.end(resultString);
-//         break;
-//     case '/getall':
-//         res.writeHead(200, {'Content-Type': 'text/plain'});
-//         let resultString2 = JSON.stringify(housing.getAll());
-//         res.end(resultString2);
-//         break;
-//     case '/delete':
-//         res.writeHead(200, {'Content-Type': 'text/plain'});
-//         let result3 = housing.delete(params.name);
-//         let resultString3 = (result3) ? "Deleted: " + JSON.stringify(result3) : "Hotel not found. Nothing deleted.";
-//         res.end(resultString3);
-//         break;
-//     default:
-//         res.writeHead(404, {'Content-Type': 'text/plain'});
-//         res.end('Not found');
-//         break;
-//     }
-// }).listen(process.env.PORT || 3000);
-//
