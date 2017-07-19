d071fefedb717fc1682903553ad8dddc9541eba8
Mike Archie <rusty505@yahoo.com>
2017-07-18 19:04:49 -0700

    adds links to hotels on all pages

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ app.set("view engine", ".html");
 
 //home page
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/home.html');
+    // res.sendFile(__dirname + '/public/home.html');
+    res.render("home", {hotels: hotels.getAll});
 });
 
 //process client hotel search using POST
 app.post('/details', function(req,res) {
     console.log(req.body.hotelSearchText);
-    var result = hotels.get(req.body.hotelSearchText);
+    let result = hotels.get(req.body.hotelSearchText);
     //render results using handlebars template (\view\details.html)
     //result contains hotel object that was found
-    res.render("details", {name: req.body.hotelSearchText, result: result, hotels: hotels.getAll()});
+    res.render("details", {name: req.body.hotelSearchText, result: result,
+        hotels: hotels.getAll()});
 });
+
+app.get('/details', function(req,res) {
+    let name = req.query.name;
+    let result = hotels.get(name);
+    res.render("details", {name: name, result: result, hotels: hotels.getAll()});
+})
 
 //process client deletion action using GET
 app.get('/delete', function(req,res) {
@@ -30,7 +38,8 @@ app.get('/delete', function(req,res) {
     let result = hotels.delete(nameToDelete);
     //render results using handlebars template (\view\delete.html)
     //result contains the remainder of the hotels array
-    res.render("delete", {name: nameToDelete, result: result, count: hotels.count()});
+    res.render("delete", {name: nameToDelete, result: result,
+        hotels: hotels.getAll(), count: hotels.count()});
 });
 
 app.listen(3000);
