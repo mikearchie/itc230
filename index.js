c11261772fb6734f00904aca2d7309f18cfb3829
Mike Archie <rusty505@yahoo.com>
2017-08-07 21:41:40 -0700

    Changes find to findOne in /details routes

--- index.js
+++ index.js
@@ -26,12 +26,14 @@ app.get('/', function(req, res) {
 //process client hotel search using POST (e.g. search result)
 app.post('/details', function(req,res) {
     let searchName = req.body.hotelSearchText;
+    //create regex to ignore lowercase
+    let nameRegExp = new RegExp(searchName, "i");
     // let result = hotels.get(searchName);
-    Hotel.find({name: searchName}, function(err, result) {
+    Hotel.findOne({name: {$regex: nameRegExp}}, function(err, hotel) {
         Hotel.find(function(err,items) {
             //render results using handlebars template (\view\details.html)
             //result contains hotel object that was found
-            res.render("details", {name: searchName, result: result[0], hotels: items});
+            res.render("details", {name: searchName, result: hotel, hotels: items});
         });
     });
     // res.render("details", {name: searchName, result: result, hotels: hotels.getAll()});
@@ -40,24 +42,27 @@ app.post('/details', function(req,res) {
 //process client hotel selection using GET (e.g. clicking a link)
 app.get('/details', function(req,res) {
     let queryName = req.query.name;
+    let nameRegExp = new RegExp(queryName, "i");
     // let result = hotels.get(name);
     //render results using handlebars template (\view\details.html)
     //result contains hotel object that was found
-    Hotel.find({name: queryName}, function(err, result) {
+    Hotel.findOne({name: {$regex: nameRegExp}}, function(err, hotel) {
         Hotel.find(function(err,items) {
-            res.render("details", {name: queryName, result: result[0], hotels: items});
+            res.render("details", {name: queryName, result: hotel, hotels: items});
         });
     });
 });
 
-// //process client deletion action using GET
-// app.get('/delete', function(req,res) {
-//     let nameToDelete = req.query.name;
-//     let result = hotels.delete(nameToDelete);
-//     //render results using handlebars template (\view\delete.html)
-//     //result contains the remainder of the hotels array
-//     res.render("delete", {name: nameToDelete, result: result,
-//         hotels: hotels.getAll(), count: hotels.count()});
-// });
+//process client deletion action using GET
+app.get('/delete', function(req,res) {
+    let nameToDelete = req.query.name;
+    // let result = hotels.delete(nameToDelete);
+
+    //render results using handlebars template (\view\delete.html)
+    //result contains the remainder of the hotels array
+
+    res.render("delete", {name: nameToDelete, result: result,
+        hotels: hotels.getAll(), count: hotels.count()});
+});
 
 app.listen(3000);
